a234ac9b660b75f4a4a183a21dcc47a4363b913e
Everton Moraes <everton.moraes@ibstecnologia.com.br>
2021-05-13 21:19:24 -0300

    Config da database

---
+++ src/config/database-example.js
@@ -0,0 +1,14 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  port:"5433",
+  username: "",
+  password: "",
+  database: "",
+  define: {
+    timestamps: true,
+    underscored: true
+  },
+  // logging: false,
+  SECRET: "",
+};
